d551bc787373dfd31391757233feee45eff5ce26
Leonardo Alfonso Cruz Rodríguez <alu0101233093@ull.edu.es>
2024-06-06 23:34:59 +0200

    HTML de perfil terminado

--- src/app/components/pages/profile/profile.component.ts
+++ src/app/components/pages/profile/profile.component.ts
@@ -10,6 +10,9 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class ProfileComponent {
   userProfile: userProfile = {} as userProfile
+  editedUserProfile: userProfile = {} as userProfile
+  uploadedFile: File | null = null;
+  readMode: boolean = true;
 
   constructor(private auth: AuthService, private router: Router){}
 
@@ -17,9 +20,35 @@ export class ProfileComponent {
     this.auth.getUserProfile()
     .then((user) => {
       this.userProfile = user;
+      this.editedUserProfile = user;
     }).catch(() => {
       this.router.navigate(['/home'])
     })
+  }
+  
+  changeMode(){
+    this.editedUserProfile = {...this.userProfile}
+    this.uploadedFile = null;
+    this.readMode = !this.readMode
+  }
+
+  onFileSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      this.uploadedFile = input.files[0];
+    }
+  }
+
+  updateProfile(){
+    
+  }
+
+  requestVerification(){
+
+  }
+
+  deleteAccount(){
+
   }
 
   logOut(){
